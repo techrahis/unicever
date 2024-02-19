6109399f5c06cb5eda33ebd85c539858d80692ea
Rajarshi Samaddar <itsrajarshi@gmail.com>
2024-02-20 01:02:20 +0530

    fix: UI branding

--- app/auth/_components/header.tsx
+++ app/auth/_components/header.tsx
@@ -14,7 +14,15 @@ interface HeaderProps {
 export const Header = ({ label }: HeaderProps) => {
   return (
     <div className="w-full flex flex-col gap-y-4 items-center justify-center">
-      <h1 className={cn("text-3xl font-semibold", font.className)}>Unicever</h1>
+      <h1
+        className={cn(
+          "text-3xl font-semibold tracking-widest text-blue-900 dark:text-white",
+          font.className
+        )}
+      >
+        <span className="bg-blue-900 rounded-md px-2 mr-1 text-white">Uni</span>
+        cever
+      </h1>
       <p className="text-muted-foreground text-sm">{label}</p>
     </div>
   );
