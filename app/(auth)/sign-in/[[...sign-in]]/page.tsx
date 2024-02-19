9837e4b38af6a516dc534307cf839fbb6555ef89
Rajarshi Samaddar <itsrajarshi@gmail.com>
2024-02-19 20:45:27 +0530

    chore: fresh project init

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
-
-export default function Page() {
-  return (
-    <section className="flex justify-center items-center h-screen">
-      <SignIn />
-    </section>
-  );
-}
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++
@@ -1,8 +0,0 @@
-import { SignUp } from "@clerk/nextjs";
-export default function Page() {
-  return (
-    <section className="flex justify-center items-center h-screen">
-      <SignUp />
-    </section>
-  );
-}
