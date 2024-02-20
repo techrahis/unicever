97cb6fb943ee55607e3ac860a7d84568a40e6f0a
Rajarshi Samaddar <itsrajarshi@gmail.com>
2024-02-20 11:59:18 +0530

    feat: auth ground work

---
+++ app/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Page</h1>
+      <p>
+        This is a page that is protected by authentication. You can only see
+        this page if you are logged in.
+      </p>
+    </div>
+  );
+}
