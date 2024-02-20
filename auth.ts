d9d91262217f43dd8d8609bcb767cdd66945731c
Rajarshi Samaddar <itsrajarshi@gmail.com>
2024-02-20 11:44:50 +0530

    feat: next auth config

---
+++ auth.ts
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth";
+import authConfig from "@/auth.config";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "@/lib/prisma";
+
+export const {
+  handlers: { GET, POST },
+  auth,
+} = NextAuth({
+  adapter: PrismaAdapter(prisma) as any,
+  session: {
+    strategy: "jwt",
+  },
+  ...authConfig,
+});
---
+++ middleware.ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth";
+import authConfig from "@/auth.config";
+
+const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+  // req.auth
+  console.log("Route: ", req.nextUrl.pathname);
+});
+
+// Optionally, don't invoke Middleware on some paths
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
