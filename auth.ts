42dbafa6d85ae080c72a9c2d7a37b2d51760e04e
Rajarshi Samaddar <itsrajarshi@gmail.com>
2024-02-20 17:04:06 +0530

    feat: rba

--- auth.ts
+++ auth.ts
@@ -1,7 +1,9 @@
 import NextAuth from "next-auth";
 import authConfig from "@/auth.config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import { UserRole } from "@prisma/client";
 import prisma from "@/lib/prisma";
+import { getUserById } from "@/lib/get-user-by-id";
 
 export const {
   handlers: { GET, POST },
@@ -9,9 +11,34 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
   adapter: PrismaAdapter(prisma) as any,
   session: {
     strategy: "jwt",
+    maxAge: 2 * 60 * 60, // 2 hours
   },
   ...authConfig,
 });
