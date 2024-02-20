f3bdc7b40cbdace034cb9f818ecec8a3d5c8bc9a
Rajarshi Samaddar <itsrajarshi@gmail.com>
2024-02-20 18:05:58 +0530

    fix: home page image

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,18 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
+      <p className="text-xl font-bold">
+        UNICEVER is an open-source project that revolutionizes certificate
+        authentication and verification. Empower educational institutions and
+        organizations to issue and validate tamper-proof digital credentials
+        effortlessly. Implement secure issuance, automated verification, and
+        user-friendly access.
+      </p>
       <Image
-        src="/vercel.svg"
-        alt="Vercel Logo"
-        width={72}
-        height={16}
+        src="/logo.svg"
+        alt="Unicever Logo"
+        width={350}
+        height={350}
         className="mt-4"
       />
     </main>
