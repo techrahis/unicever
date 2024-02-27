60c9923f69a1b89e4797a6ce025c317bebf6461f
Rajarshi Samaddar <itsrajarshi@gmail.com>
2024-02-27 23:07:14 +0530

    feat: profile page actions

---
+++ server/profile-update.ts
@@ -0,0 +1,75 @@
+"use server";
+
+import * as z from "zod";
+import { AccountSchemaBackend } from "@/schemas/profile";
+import prisma from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
+import { StorageClient } from "@supabase/storage-js";
+
+const STORAGE_URL = process.env.STORAGE_URL || "";
+const SERVICE_KEY = process.env.SERVICE_KEY || "";
+const storageClient = new StorageClient(STORAGE_URL, {
+  apikey: SERVICE_KEY,
+  Authorization: `Bearer ${SERVICE_KEY}`,
+});
+
+export const updateAccountAction = async (
+  data: z.infer<typeof AccountSchemaBackend>
+) => {
+  const validatedData = AccountSchemaBackend.safeParse(data);
+
+  if (!validatedData.success) {
+    return { error: "Invalid fields!" };
+  }
+
+  const { name, phone, id, photo } = validatedData.data;
+
+  if (photo) {
+    try {
+      const timestamp = new Date().getTime();
+      // Convert the base64 string to a Blob
+      const base64String = `data:image/png;base64,${photo}`;
+      const response = await fetch(base64String);
+      const blob = await response.blob();
+
+      // Wait for the upload to finish
+      await storageClient
+        .from("s1-dev/avatar")
+        .upload(`${id}_${timestamp}.png`, blob, {
+          cacheControl: "3600",
+          contentType: "image/png",
+        });
+
+      await prisma.user.update({
+        where: {
+          id: id,
+        },
+        data: {
+          name: name,
+          phone: phone,
+          image: `https://vlyhguzcuqmgcwvxirun.supabase.co/storage/v1/object/public/s1-dev/avatar/${id}_${timestamp}.png`,
+        },
+      });
+      revalidatePath("/app/profile");
+      return { success: "Profile updated successfully!" };
+    } catch (error) {
+      return { error: "Something went wrong!" };
+    }
+  } else {
+    try {
+      await prisma.user.update({
+        where: {
+          id: id,
+        },
+        data: {
+          name: name,
+          phone: phone,
+        },
+      });
+      revalidatePath("/app/profile");
+      return { success: "Profile updated successfully!" };
+    } catch (error) {
+      return { error: "Something went wrong!" };
+    }
+  }
+};
