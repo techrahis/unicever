c28862b558bef9fd372a92f2e5f771d6dc19cb40
Rajarshi Samaddar <itsrajarshi@gmail.com>
2024-02-27 23:06:39 +0530

    feat: profile page actions

--- app/app/profile/_components/account-card.tsx
+++ app/app/profile/_components/account-card.tsx
@@ -1,5 +1,16 @@
-import { auth } from "@/auth";
-import { Button } from "@/components/ui/button";
+"use client";
+
+import React, { useTransition } from "react";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormItem,
+  FormMessage,
+  FormField,
+} from "@/components/ui/form";
 import {
   Card,
   CardContent,
@@ -8,19 +19,86 @@ import {
   CardHeader,
   CardFooter,
 } from "@/components/ui/card";
+import { AccountSchema } from "@/schemas/profile";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
 import { CheckIcon } from "@radix-ui/react-icons";
-import prisma from "@/lib/prisma";
+import { updateAccountAction } from "@/server/profile-update";
+import { useToast } from "@/components/ui/use-toast";
 
-export default async function AccountCard() {
-  const session = await auth();
-  const user = await prisma.user.findUnique({
-    where: {
-      id: session?.user.id,
+type UserProps = {
+  id: string | undefined;
+  name: string | null;
+  phone: string | null;
+  image: string | null;
+  email: string | null;
+} | null;
+
+export default function AccountCard({ user }: { user: UserProps }) {
+  const [isPending, startTransition] = useTransition();
+  const { toast } = useToast();
+
+  const form = useForm<z.infer<typeof AccountSchema>>({
+    resolver: zodResolver(AccountSchema),
+    defaultValues: {
+      id: user?.id || undefined,
+      photo: "",
+      phone: user?.phone || "",
+      name: user?.name || "",
     },
   });
+
+  const accountAction = (data: z.infer<typeof AccountSchema>) => {
+    const photoFile = form.getValues("photo")[0];
+
+    // date time in format of Friday, 2 April 2021 12:00:00
+
+    const timestamp = new Date().getTime();
+
+    const date = new Date(timestamp);
+
+    const options: Intl.DateTimeFormatOptions = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    };
+
+    const formattedDate = new Intl.DateTimeFormat("en-US", options).format(
+      date
+    );
+
+    if (photoFile instanceof File) {
+      const reader = new FileReader();
+      reader.onloadend = async () => {
+        const base64String = reader.result as string;
+        const base64Data = base64String.split(",")[1];
+        startTransition(() => {
+          updateAccountAction({ ...data, photo: base64Data }).then((data) => {
+            data?.success &&
+              toast({
+                title: data?.success,
+                description: formattedDate,
+              });
+            data?.error &&
+              toast({
+                title: data?.error,
+                description: formattedDate,
+              });
+          });
+        });
+      };
+      reader.readAsDataURL(photoFile);
+    } else {
+      updateAccountAction(data);
+    }
+  };
+
   return (
     <Card className="mt-4">
       <CardHeader>
@@ -30,60 +108,118 @@ export default async function AccountCard() {
           done.
         </CardDescription>
       </CardHeader>
-      <CardContent className="space-y-4">
-        <section className="flex flex-col justify-between space-y-4 sm:space-y-0 sm:space-x-4 sm:flex-row">
-          <div className="space-y-1 w-full">
-            <Label htmlFor="picture">Profile picture</Label>
-            <section className="flex space-x-2 items-center">
-              <Avatar>
-                <AvatarImage src={user?.image || undefined} />
-                <AvatarFallback className="uppercase font-bold">
-                  {user?.name
-                    ? user.name.split(" ").length > 1
-                      ? user.name
-                          .split(" ")
-                          .slice(0, 2)
-                          .map((name) => name[0])
-                          .join("")
-                      : user.name.slice(0, 2)
-                    : ""}
-                </AvatarFallback>
-              </Avatar>
-              <Input id="picture" type="file" />
-            </section>
-          </div>
-          <div className="space-y-1 w-full">
-            <Label htmlFor="phone">Phone</Label>
-            <Input
-              type="number"
-              id="phone"
-              defaultValue={user?.phone || undefined}
-            />
-          </div>
-        </section>
 
-        <section className="flex flex-col justify-between space-y-4 sm:space-y-0 sm:space-x-4 sm:flex-row">
-          <div className="space-y-1 w-full">
-            <Label htmlFor="name">Name</Label>
-            <Input id="name" defaultValue={session?.user.name || undefined} />
-          </div>
-          <div className="space-y-1 w-full">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              type="email"
-              id="email"
-              defaultValue={session?.user.email || undefined}
-              disabled
-            />
-          </div>
-        </section>
-      </CardContent>
-      <CardFooter>
-        <Button type="submit">
-          <CheckIcon className="w-4 h-4 mr-2" />
-          Save changes
-        </Button>
-      </CardFooter>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(accountAction)}>
+          <CardContent className="space-y-4">
+            <section className="flex flex-col justify-between space-y-4 sm:space-y-0 sm:space-x-4 sm:flex-row">
+              <div className="space-y-1 w-full">
+                <FormField
+                  name="photo"
+                  render={() => (
+                    <FormItem>
+                      <Label htmlFor="photo">Profile picture</Label>
+                      <section className="flex space-x-2 items-center">
+                        <Avatar>
+                          <AvatarImage src={user?.image || undefined} />
+                          <AvatarFallback className="uppercase font-bold">
+                            {user?.name
+                              ? user.name.split(" ").length > 1
+                                ? user.name
+                                    .split(" ")
+                                    .slice(0, 2)
+                                    .map((name) => name[0])
+                                    .join("")
+                                : user.name.slice(0, 2)
+                              : ""}
+                          </AvatarFallback>
+                        </Avatar>
+                        <FormControl>
+                          <Input
+                            id="photo"
+                            defaultValue={"xys"}
+                            {...form.register("photo")}
+                            type="file"
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </section>
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <div className="space-y-1 w-full">
+                <FormField
+                  control={form.control}
+                  name="phone"
+                  render={({ field }) => (
+                    <FormItem>
+                      <Label htmlFor="phone">Phone</Label>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          type="number"
+                          className="[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </section>
+
+            <section className="flex flex-col justify-between items-center space-y-4 sm:space-y-0 sm:space-x-4 sm:flex-row">
+              <div className="space-y-1 w-full">
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <Label htmlFor="name">Name</Label>
+                      <FormControl>
+                        <Input {...field} type="text" />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="space-y-1 w-full">
+                <FormField
+                  name="email"
+                  render={() => (
+                    <FormItem>
+                      <Label htmlFor="name">Email</Label>
+                      <Input
+                        type="email"
+                        disabled={true}
+                        placeholder={user?.email || ""}
+                      />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </section>
+          </CardContent>
+          <FormField
+            control={form.control}
+            name="id"
+            render={({ field }) => (
+              <FormItem>
+                <Input {...field} type="hidden" />
+              </FormItem>
+            )}
+          />
+          <CardFooter>
+            <Button disabled={isPending} type="submit">
+              <CheckIcon className="w-4 h-4 mr-2" />
+              Save changes
+            </Button>
+          </CardFooter>
+        </form>
+      </Form>
     </Card>
   );
 }
