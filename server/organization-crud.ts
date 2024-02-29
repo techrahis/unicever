7e6b9e9faa2fc8d670f4cfdc0bcfbf86d470473c
soumayadip2002 <sinuroy696@gmail.com>
2024-02-29 16:15:38 +0530

    added supabase storage to server actions

--- server/organization-crud.ts
+++ server/organization-crud.ts
@@ -5,26 +5,45 @@ import { z } from "zod";
 import prisma from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
 import { Prisma } from "@prisma/client";
+import storageClient from "@/lib/storageClient";
+import { organizationType } from "@/types/organizationType";
 
 //create organization for first time
-export const OrganizationCreate = async (
-  values: z.infer<typeof OrganizationSchema>
-) => {
+export const OrganizationCreate = async (formData: FormData) => {
   try {
-    //validating data
+    const values: Record<string, any> = {};
+    //converting form data to objcet
+    formData.forEach((value, key) => {
+      values[key] = value;
+    });
     const validateData = OrganizationSchema.safeParse(values);
     if (!validateData.success) {
-      console.log("please check");
-      return { meesage: "Please Chcek your data", variant: "error" };
+      return { message: "Please Chcek your data", variant: "error" };
     }
     const { userId, name, phone, email, description, address, logo } =
       validateData.data;
 
-    const logoDetails = {
-      src: `https://vlyhguzcuqmgcwvxirun.supabase.co/storage/v1/object/public/${logo.fullPath}`,
-      id: logo.id,
-      path: logo.path,
-    };
+    let logoDetails = {};
+    //checking weather logo is a file or not
+    if (logo instanceof File) {
+      //uploading file
+      const { data, error } = await storageClient
+        .from("s1-dev/avatar")
+        .upload(`${userId}_${logo.name}`, logo, {
+          cacheControl: "3600",
+        });
+      if (error) return { message: "something went wrong", variant: "error" };
+
+      //updating logoDetails with uploaded file data
+      logoDetails = {
+        src: `https://vlyhguzcuqmgcwvxirun.supabase.co/storage/v1/object/public/${
+          (data as any).fullPath
+        }`,
+        id: (data as any).id,
+        path: data.path,
+      };
+    }
+
     //creating organization
     await prisma.organization.create({
       data: {
@@ -34,7 +53,7 @@ export const OrganizationCreate = async (
         email,
         description,
         address,
-        logo: JSON.stringify(logoDetails),
+        logo: logoDetails,
       },
     });
     revalidatePath("/app/profile");
@@ -49,29 +68,64 @@ export const OrganizationCreate = async (
 
 //update organization
 export const OrganizationUpdate = async ({
-  values,
-  id,
+  formData,
+  existOrganization,
 }: {
-  values: z.infer<typeof OrganizationSchema>;
-  id: string;
+  formData: FormData;
+  existOrganization: organizationType;
 }) => {
   try {
-    const validateData = OrganizationSchema.safeParse(values);
-    if (!validateData.success) {
-      console.log("please check");
+    const values: Record<string, any> = {};
+    //converting form data to objcet
+    formData.forEach((value, key) => {
+      values[key] = value;
+    });
+
+    //validating data
+    const validate = OrganizationSchema.safeParse(values);
+    if (!validate.success) {
       return { message: "Please Chcek your data", variant: "error" };
     }
-    const { name, phone, email, description, address, logo } =
-      validateData.data;
 
-    const logoDetails = {
-      src:`https://${process.env.DATABASE_NAME}.supabase.co/storage/v1/object/public/${logo.fullPath}`,
-      id: logo.id,
-      path: logo.path,
-    };
+    //getting data from validation data
+    const { userId, name, phone, email, address, description, logo, image } =
+      validate.data;
+
+    //getting exist organization logo details
+    let logoDetails = existOrganization.logo!;
+
+    //if user change file or upload new one
+    if (logo instanceof File) {
+      //deleting exist file
+      if (typeof logoDetails === "object" && "path" in logoDetails) {
+        await storageClient
+          .from("s1-dev")
+          .remove([`avatar/${logoDetails.path}`]);
+      }
+
+      //uploading new one
+      const { data, error } = await storageClient
+        .from("s1-dev/avatar")
+        .upload(`${userId}_${logo.name}`, logo, {
+          cacheControl: "3600",
+        });
+
+      //if any error occured
+      if (error) return { message: "something went wrong", variant: "error" };
+      //updating logoDetails with new uploading data
+      logoDetails = {
+        src: `https://${
+          process.env.DATABASE_NAME
+        }.supabase.co/storage/v1/object/public/${(data as any).fullPath}`,
+        id: (data as any).id,
+        path: data.path,
+      };
+    }
+
+    //updating data
     await prisma.organization.update({
       where: {
-        id: id,
+        id: existOrganization.id,
       },
       data: {
         name,
@@ -79,7 +133,7 @@ export const OrganizationUpdate = async ({
         email,
         description,
         address,
-        logo: JSON.stringify(logoDetails),
+        logo: logoDetails!,
       },
     });
     revalidatePath("/app/profile");
