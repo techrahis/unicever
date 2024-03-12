01739370c39e3e79c491964714a2e949c3c56ee6
soumayadip2002 <sinuroy696@gmail.com>
2024-03-12 21:23:39 +0530

    refactor

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -11,9 +11,13 @@ import prisma from "@/lib/prisma";
 import { getOrganization } from "@/server/organization-crud";
 import Link from "next/link";
 import * as uuid from "uuid";
+import NoOrganization from "./_components/NoOrganization";
 export default async function Page() {
   const session = await auth();
   const organization = await getOrganization(session?.user.id!);
+  if (!organization) {
+    return <NoOrganization />;
+  }
   const events = await prisma.event.findMany({
     where: {
       organizationId: organization?.id,
@@ -31,40 +35,43 @@ export default async function Page() {
           Create Event
         </Link>
       </div>
-      <div className="mt-4 grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-        {events.length > 0
-          ? events.map((event) => (
-              <Card key={event.id}>
-                <CardHeader>
-                  <CardTitle>{event.title}</CardTitle>
-                  <div className="text-[0.8rem] text-muted-foreground flex justify-between items-center">
-                    <p className="text-primary">{event.location}</p>
-                    <p>{event.date.toLocaleDateString()}</p>
-                  </div>
-                </CardHeader>
-                <CardContent>
-                  <p className="text-sm font-normal">
-                    {event.description?.slice(0, 80) + "..."}
-                  </p>
-                </CardContent>
-                <CardFooter className="flex items-center gap-2">
-                  <Link
-                    href={`/app/event/${event.id}`}
-                    className={buttonVariants({ variant: "secondary" })}
-                  >
-                    Manage
-                  </Link>
-                  <Link
-                    href={`/app/create-event/${event.id}`}
-                    className={buttonVariants({ variant: "outline" })}
-                  >
-                    Edit
-                  </Link>
-                </CardFooter>
-              </Card>
-            ))
-          : ""}
-      </div>
+
+      {events.length > 0 ? (
+        <div className="mt-4 grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+          {events.map((event) => (
+            <Card key={event.id}>
+              <CardHeader>
+                <CardTitle>{event.title}</CardTitle>
+                <div className="text-[0.8rem] text-muted-foreground flex justify-between items-center">
+                  <p className="text-primary">{event.location}</p>
+                  <p>{event.date.toLocaleDateString()}</p>
+                </div>
+              </CardHeader>
+              <CardContent>
+                <p className="text-sm font-normal">
+                  {event.description?.slice(0, 80) + "..."}
+                </p>
+              </CardContent>
+              <CardFooter className="flex items-center gap-2">
+                <Link
+                  href={`/app/event/${event.id}`}
+                  className={buttonVariants({ variant: "secondary" })}
+                >
+                  Manage
+                </Link>
+                <Link
+                  href={`/app/create-event/${event.id}`}
+                  className={buttonVariants({ variant: "outline" })}
+                >
+                  Edit
+                </Link>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
