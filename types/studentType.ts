c8dd26a8eaf6e37b23d3641a2648fbfd1571c360
soumayadip2002 <sinuroy696@gmail.com>
2024-03-25 10:04:53 +0530

    refactor

--- types/studentType.ts
+++ types/studentType.ts
@@ -1,7 +1,14 @@
+import { JsonValue } from "@prisma/client/runtime/library"
+
+export type certificateType = {
+    id: string | null,
+    path: string | null,
+    src: string | null,
+}
 export type studentType = {
     id:string | null,
     name:string | null,
-    regNo:string | null,
-    certificate:string | null,
+    studentId:string | null,
+    certificateData:certificateType | JsonValue | null,
     eventId:string | null
 }
